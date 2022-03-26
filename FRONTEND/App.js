1934806d7d537d3896bb55a54c11c1b9c1138cc0
Mustaf <mustafdiiriye@hotmail.com>
2022-03-26 15:02:10 +0300

    Structure Application

--- FRONTEND/App.js
+++ FRONTEND/App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ProductContainer from './Screens/Products/ProductContainer';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Eshop</Text>
+      <ProductContainer/>
       <StatusBar style="auto" />
     </View>
   );
---
+++ FRONTEND/Screens/Products/ProductContainer.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ProductContainer = () => {
+  return (
+    <View>
+      <Text>Product Container</Text>
+    </View>
+  )
+}
+
+export default ProductContainer
+
+const styles = StyleSheet.create({})
