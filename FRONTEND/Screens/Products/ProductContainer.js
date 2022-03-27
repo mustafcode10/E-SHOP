cf9e5722325ffd82445fb2591738f3fabd99f4f5
Mustaf <mustafdiiriye@hotmail.com>
2022-03-27 19:08:35 +0300

    search product

--- FRONTEND/Screens/Products/ProductContainer.js
+++ FRONTEND/Screens/Products/ProductContainer.js
@@ -1,30 +1,61 @@
 import React, { useState, useEffect } from "react";
 import {
   StyleSheet,
-  Text,
   View,
   ActivityIndicator,
   FlatList,
+  Text,
+  TextInput,
+  Dimensions,
+  SafeAreaView,
 } from "react-native";
+import { SearchBar } from "react-native-elements";
+import { Ionicons } from "react-native-vector-icons";
 
-import ProductList from "./ProductList"
+import ProductList from "./ProductList";
 const data = require("./../../assets/data/products.json");
+var { width } = Dimensions.get("window");
 
 const ProductContainer = () => {
   const [products, setProducts] = useState([]);
+  const [productsFilter, setProductsFilter] = useState([]);
   useEffect(() => {
     setProducts(data);
-  });
+    setProductsFilter(data);
+    return () => {
+      setProducts([]);
+    }
+  },[]);
   return (
     <View>
+      {/* <SearchBar
+        placeholder="Type Here..."
+      /> */}
+      <View
+        style={{
+          flexDirection: "row",
+          backgroundColor: "#d3d3d3",
+          width: width,
+          height: 40,
+          // justifyContent: "center",
+          alignItems: "center",
+          borderRadius: width / 2,
+          padding: 5,
+        }}
+      >
+        <Ionicons name="ios-search" size={25} color="black" />
+        <TextInput
+          placeholder="Search"
+          style={{ fontSize: 15, fontWeight: "bold", padding: 5 }}
+        />
+      </View>
       <Text>Product Container</Text>
       <View style={{ marginTop: 100 }}>
         <FlatList
           numColumns={2}
-         
           data={products}
           keyExtractor={(item) => item.id}
-          renderItem={({ item }) => <ProductList item={item} key={item.id}/>}
+          renderItem={({ item }) => <ProductList item={item} key={item.id} />}
         />
       </View>
     </View>
@@ -33,4 +64,6 @@ const ProductContainer = () => {
 
 export default ProductContainer;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+ 
+});
---
+++ FRONTEND/Screens/Products/SearchedProducts.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SearchedProducts = () => {
+  return (
+    <View>
+      <Text>SearchedProducts</Text>
+    </View>
+  )
+}
+
+export default SearchedProducts
+
+const styles = StyleSheet.create({})
