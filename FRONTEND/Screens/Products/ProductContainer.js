edd0a3674da78f675f36c04bcb05185c1604e43d
Mustaf <mustafdiiriye@hotmail.com>
2022-03-27 20:25:36 +0300

    search product

--- FRONTEND/Screens/Products/ProductContainer.js
+++ FRONTEND/Screens/Products/ProductContainer.js
@@ -8,65 +8,92 @@ import {
   TextInput,
   Dimensions,
   SafeAreaView,
+  ScrollView,
 } from "react-native";
 import { SearchBar } from "react-native-elements";
 import { Ionicons } from "react-native-vector-icons";
 
 import ProductList from "./ProductList";
-import SearchedProducts from './SearchedProducts';
+import SearchedProducts from "./SearchedProducts";
 const data = require("./../../assets/data/products.json");
 var { width } = Dimensions.get("window");
 
 const ProductContainer = () => {
   const [products, setProducts] = useState([]);
   const [productsFiltered, setProductsFiltered] = useState([]);
+  const [focus, setFocus] = useState();
   useEffect(() => {
     setProducts(data);
     setProductsFiltered(data);
+    setFocus(false);
     return () => {
       setProducts([]);
-    }
-  },[]);
+      setProductsFiltered([]);
+      setFocus();
+    };
+  }, []);
+  const searchProduct = (text) => {
+    setProductsFiltered(
+      products.filter((item) =>
+        item.name.toLowerCase().includes(text.toLowerCase())
+      )
+    );
+  };
+  const openList = () => {
+    setFocus(true);
+  };
+  const onBlur = () => {
+    setFocus(false);
+  };
   return (
-    <View >
-      {/* <SearchBar
+    <ScrollView>
+      <View>
+        {/* <SearchBar
         placeholder="Type Here..."
       /> */}
-      <View
-        style={{
-          flexDirection: "row",
-          backgroundColor: "#d3d3d3",
-          width: width,
-          height: 40,
-          // justifyContent: "center",
-          alignItems: "center",
-          borderRadius: width / 2,
-          padding: 5,
-        }}
-      >
-        <Ionicons name="ios-search" size={25} color="black" />
-        <TextInput
-          placeholder="Search"
-          style={{ fontSize: 15, fontWeight: "bold", padding: 5 }}
-        />
+        <View
+          style={{
+            flexDirection: "row",
+            backgroundColor: "#d3d3d3",
+            width: width,
+            height: 40,
+            // justifyContent: "center",
+            alignItems: "center",
+            borderRadius: width / 2,
+            padding: 5,
+          }}
+        >
+          <Ionicons name="ios-search" size={25} color="black" />
+          <TextInput
+            placeholder="Search"
+            style={{ fontSize: 15, fontWeight: "bold", padding: 5 }}
+            onChangeText={(text) => searchProduct(text)}
+            onFocus={openList}
+          />
+        </View>
+        {focus == true ? (
+          <SearchedProducts
+            productsFiltered={productsFiltered}
+            onBlur={onBlur}
+          />
+        ) : (
+          <View style={{ marginTop: 100 }}>
+            <Text>Product Container</Text>
+            <FlatList
+              numColumns={2}
+              data={products}
+              keyExtractor={(item) => item.id}
+              renderItem={({ item }) => (
+                <ProductList item={item} key={item.id} />
+              )}
+            />
+          </View>
+        )}
       </View>
-      <Text>Product Container</Text>
-      <View style={{ marginTop: 100 }}>
-        <FlatList
-          numColumns={2}
-          data={products}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => <ProductList item={item} key={item.id} />}
-        />
-     
-      </View>
-    </View>
+    </ScrollView>
   );
 };
 
 export default ProductContainer;
 
-const styles = StyleSheet.create({
-
- 
-});
+const styles = StyleSheet.create({});
--- FRONTEND/Screens/Products/SearchedProducts.js
+++ FRONTEND/Screens/Products/SearchedProducts.js
@@ -1,10 +1,24 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const SearchedProducts = () => {
+const SearchedProducts = (props) => {
+    const {productsFiltered} = props;
   return (
     <View>
       <Text>SearchedProducts</Text>
+      {productsFiltered.length > 0 ? (
+          productsFiltered.map((item, index) => (
+              <View key={index}>
+                    <Text>{item.name}</Text>
+              </View>
+          )
+
+          )
+      ): (
+          <View>
+              <Text>No products found</Text>
+          </View>
+      )}
     </View>
   )
 }
