c162eaa17150408d0c740b4a22df2247c5d2ae2a
Mustaf <mustafdiiriye@hotmail.com>
2022-04-06 14:44:27 +0300

    Checkout Navigator tabs

--- FRONTEND/Navigators/CartNavigator.js
+++ FRONTEND/Navigators/CartNavigator.js
@@ -1,39 +1,34 @@
+import React from "react";
 
-import React from 'react'
-
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator } from "@react-navigation/stack";
 
 import Cart from "./../Screens/Cart/Cart";
-// import Checkout from './../Screens/Cart/Checkout.js/Checkout';
-import Checkout from './../Screens/Cart/Checkout/Checkout'
+import CheckoutNavigator from "./CheckoutNavigator";
 
 const Stack = createStackNavigator();
 
 const MyStack = () => {
-    return (
-        <Stack.Navigator>
-        <Stack.Screen name="Cart"
-            component={Cart}
-            options={{
-            headerShown: false
-            }}
-        />
-                <Stack.Screen name="Checkout"
-            component={Checkout}
-            options={{
-            headerShown: false
-            }}
-        />
-        </Stack.Navigator>
-    )
-}
-const CartNavigator = () => {
   return (
-   <MyStack/>
-  )
-}
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Cart"
+        component={Cart}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="Checkout"
+        component={CheckoutNavigator}
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
+const CartNavigator = () => {
+  return <MyStack />;
+};
 
-
-
-export default CartNavigator
-
+export default CartNavigator;
