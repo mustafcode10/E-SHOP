59f9bf2ee4c1b43d2fe97f9a3d5b8d2e8a73a45f
Mustaf <mustafdiiriye@hotmail.com>
2022-04-07 16:38:04 +0300

    shipping orders

--- FRONTEND/Screens/Cart/Checkout/Checkout.js
+++ FRONTEND/Screens/Cart/Checkout/Checkout.js
@@ -3,8 +3,12 @@ import { StyleSheet, Text, View, Picker, Button } from "react-native";
 import FormContainer from "./../../../Shared/Form/FormContainer";
 import Input from "./../../../Shared/Form/Input";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { FontAwesome } from "react-native-vector-icons";
 
-const Checkout = () => {
+const countries = require("./../../../assets/countries.json");
+import { connect } from "react-redux";
+
+const Checkout = (props) => {
   const [orderItems, setOrderItems] = useState();
   const [address, setAddress] = useState();
   const [address2, setAddress2] = useState();
@@ -13,6 +17,29 @@ const Checkout = () => {
   const [country, setCountry] = useState();
   const [phone, setPhone] = useState();
   const [user, setUser] = useState();
+
+  useEffect(() => {
+    setOrderItems(props.cartItems);
+    return () => {
+      setOrderItems();
+    };
+  }, []);
+
+  const checkOut = () => {
+    console.log("orders", orderItems)
+    let order = {
+        city,
+        country,
+        dateOrdered: Date.now(),
+        orderItems,
+        phone,
+        shippingAddress1: address,
+        shippingAddress2: address2,
+        zip,
+    }
+
+    props.navigation.navigate("Payment", {order: order })
+}
 
   return (
     <KeyboardAwareScrollView
@@ -53,6 +80,17 @@ const Checkout = () => {
           keyboardType={"numeric"}
           onChangeText={(text) => setZip(text)}
         />
+        <Picker
+          mode="dropdown"
+          selectedValue={country}
+          style={{ height: 60, width: 200 }}
+          placeholder="Select your country"
+          onValueChange={(e) => setCountry(e)}
+        >
+          {countries.map((c) => {
+            return <Picker.Item key={c.code} label={c.name} value={c.name} />;
+          })}
+        </Picker>
         <View style={{ width: "80%", alignItems: "center" }}>
           <Button title="Confirm" onPress={() => checkOut()} />
         </View>
@@ -61,6 +99,13 @@ const Checkout = () => {
   );
 };
 
-export default Checkout;
+const mapStateToProps = (state) => {
+  const { cartItems } = state;
+  return {
+    cartItems: cartItems,
+  };
+};
+
+export default connect(mapStateToProps)(Checkout);
 
 const styles = StyleSheet.create({});
