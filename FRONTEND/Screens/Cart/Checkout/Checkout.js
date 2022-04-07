86d81d9dc6df1e2364c0db3c29b1b1777aa0344d
Mustaf <mustafdiiriye@hotmail.com>
2022-04-07 14:49:29 +0300

    shipping form

--- FRONTEND/Screens/Cart/Checkout/Checkout.js
+++ FRONTEND/Screens/Cart/Checkout/Checkout.js
@@ -1,14 +1,66 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, Picker, Button } from "react-native";
+import FormContainer from "./../../../Shared/Form/FormContainer";
+import Input from "./../../../Shared/Form/Input";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 const Checkout = () => {
+  const [orderItems, setOrderItems] = useState();
+  const [address, setAddress] = useState();
+  const [address2, setAddress2] = useState();
+  const [city, setCity] = useState();
+  const [zip, setZip] = useState();
+  const [country, setCountry] = useState();
+  const [phone, setPhone] = useState();
+  const [user, setUser] = useState();
+
   return (
-    <View>
-      <Text>Checkout payment</Text>
-    </View>
-  )
-}
+    <KeyboardAwareScrollView
+      viewIsInsideTabBar={true}
+      extraHeight={200}
+      enableOnAndroid={true}
+    >
+      <FormContainer title="Shipping Address">
+        <Input
+          placeholder={"Phone"}
+          name={"phone"}
+          value={phone}
+          keyboardType={"numeric"}
+          onChangeText={(text) => setPhone(text)}
+        />
+        <Input
+          placeholder={"Shipping Address 1"}
+          name={"ShippingAddress1"}
+          value={address}
+          onChangeText={(text) => setAddress(text)}
+        />
+        <Input
+          placeholder={"Shipping Address 2"}
+          name={"ShippingAddress2"}
+          value={address2}
+          onChangeText={(text) => setAddress2(text)}
+        />
+        <Input
+          placeholder={"City"}
+          name={"city"}
+          value={city}
+          onChangeText={(text) => setCity(text)}
+        />
+        <Input
+          placeholder={"Zip Code"}
+          name={"zip"}
+          value={zip}
+          keyboardType={"numeric"}
+          onChangeText={(text) => setZip(text)}
+        />
+        <View style={{ width: "80%", alignItems: "center" }}>
+          <Button title="Confirm" onPress={() => checkOut()} />
+        </View>
+      </FormContainer>
+    </KeyboardAwareScrollView>
+  );
+};
 
-export default Checkout
+export default Checkout;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
